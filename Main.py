352771170bbb52c8a047e2bb99bf7a711123a389
Patryk Piotrowski <piotrowski@patryk.org>
2020-03-23 22:39:14 +0100

    housekeeping

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import cv2
 import sys
 
 import time
@@ -38,71 +37,6 @@ def main(imgOriginalScene):
             print(licPlate.strChars)
 
     return
-
-
-def drawRedRectangleAroundPlate(imgOriginalScene, licPlate):
-
-    p2fRectPoints = cv2.boxPoints(licPlate.rrLocationOfPlateInScene)
-
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[0]), tuple(
-        p2fRectPoints[1]), SCALAR_RED, 2)
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[1]), tuple(
-        p2fRectPoints[2]), SCALAR_RED, 2)
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[2]), tuple(
-        p2fRectPoints[3]), SCALAR_RED, 2)
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[3]), tuple(
-        p2fRectPoints[0]), SCALAR_RED, 2)
-
-
-def writeLicensePlateCharsOnImage(imgOriginalScene, licPlate):
-    ptCenterOfTextAreaX = 0
-    ptCenterOfTextAreaY = 0
-
-    ptLowerLeftTextOriginX = 0
-    ptLowerLeftTextOriginY = 0
-
-    sceneHeight, sceneWidth, sceneNumChannels = imgOriginalScene.shape
-    plateHeight, plateWidth, plateNumChannels = licPlate.imgPlate.shape
-
-    intFontFace = cv2.FONT_HERSHEY_SIMPLEX
-    fltFontScale = float(plateHeight) / 30.0
-    intFontThickness = int(round(fltFontScale * 1.5))
-
-    textSize, baseline = cv2.getTextSize(
-        licPlate.strChars, intFontFace, fltFontScale, intFontThickness)
-
-    ((intPlateCenterX, intPlateCenterY), (intPlateWidth, intPlateHeight),
-     fltCorrectionAngleInDeg) = licPlate.rrLocationOfPlateInScene
-
-    intPlateCenterX = int(intPlateCenterX)
-    intPlateCenterY = int(intPlateCenterY)
-
-    ptCenterOfTextAreaX = int(intPlateCenterX)
-
-    if intPlateCenterY < (sceneHeight * 0.75):
-        ptCenterOfTextAreaY = int(
-            round(intPlateCenterY)) + int(round(plateHeight * 1.6))
-    else:
-        ptCenterOfTextAreaY = int(
-            round(intPlateCenterY)) - int(round(plateHeight * 1.6))
-
-    textSizeWidth, textSizeHeight = textSize
-
-    ptLowerLeftTextOriginX = int(ptCenterOfTextAreaX - (textSizeWidth / 2))
-    ptLowerLeftTextOriginY = int(ptCenterOfTextAreaY + (textSizeHeight / 2))
-
-    cv2.putText(
-        imgOriginalScene,
-        licPlate.strChars,
-        (
-            ptLowerLeftTextOriginX,
-            ptLowerLeftTextOriginY
-        ),
-        intFontFace,
-        fltFontScale,
-        SCALAR_YELLOW,
-        intFontThickness
-    )
 
 
 if __name__ == "__main__":
